54b7b83c215ddad339eb12ab3f2a419eb1b2c212
Aliyu <biggy0023@gmail.com>
2022-10-25 18:21:22 +0100

    built it into a BinarySearchTree class

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -13,43 +13,48 @@ end
 
 p arr
 
-def build_tree(arr)
-  # return nil if array is empty
-  # if array is greater than 2
-    # define the middle point of the array
-    # assign the middle point of the array as the root node of the tree
-    # define the left half of the array and convert them to the left node
-    # left nodes are smaller than root node
-    # define the right half of the array and convert them to the right node
-    # right nodes are higher than the root nodes
-  # if array is equal to  2
-    # the root node is index 0
-    # left node is nil
-    # right node is index 1
-  # if array equal 1
-    # root node is index 0
-    # execute root
-  return nil if arr.empty?
+class BinarySearchTree
+  attr_accessor :root, :data
 
-  arr_sort = arr.sort
-  array = arr_sort.uniq
-
-  if array.length > 2
-    mid_point = array.length / 2
-    root = TreeNode.new(array[mid_point])
-    root.left = build_tree(array[0...mid_point])
-    root.right = build_tree(array[mid_point + 1..- 1])
-  elsif array.length == 2
-    root = TreeNode.new(array[0])
-    root.left = nil
-    root.right = TreeNode.new(array[1])
-  elsif array.length == 1
-    root = TreeNode.new(array[0])
-  else
-    return nil
+  def initialize(array)
+    @data = array.sort.uniq
+    @root = build_tree(data)
   end
 
-  root
+  def build_tree(arr)
+    return nil if arr.empty?
+
+    arr_sort = arr.sort
+    array = arr_sort.uniq
+
+    if array.length > 2
+      mid_point = array.length / 2
+      root = TreeNode.new(array[mid_point])
+      root.left = build_tree(array[0...mid_point])
+      root.right = build_tree(array[mid_point + 1...array.length])
+    elsif array.length == 2
+      root = TreeNode.new(array[0])
+      root.left = nil
+      root.right = TreeNode.new(array[1])
+    elsif array.length == 1
+      root = TreeNode.new(array[0])
+    else
+      return nil
+    end
+    root
+  end
 end
 
-p build_tree([1,2,3,4,5,6,7,8,9])
+
+tree = BinarySearchTree.new(arr)
+p tree
+
+def insert(value)
+  # convert value to node
+  # if value is less than root, move it to the left to the lowest node
+  # if value is greater than root, move it to the right to the lowest node
+  node = TreeNode.new(value)
+
+end
+
+# p insert(20)
