1951525e5f5ae4d6bf4535090791ba7e226c0ae6
Aliyu <biggy0023@gmail.com>
2022-10-25 17:08:56 +0100

    successfully create the binary tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -4,7 +4,7 @@ require_relative 'node'
 
 # Binary search tree
 arr = []
-count = rand(5..30) + 1
+count = rand(5..10) + 1
 
 until count.zero?
   arr << rand(0..1000) + 1
@@ -13,7 +13,7 @@ end
 
 p arr
 
-def build_tree(array)
+def build_tree(arr)
   # return nil if array is empty
   # if array is greater than 2
     # define the middle point of the array
@@ -29,7 +29,27 @@ def build_tree(array)
   # if array equal 1
     # root node is index 0
     # execute root
-  
+  return nil if arr.empty?
+
+  arr_sort = arr.sort
+  array = arr_sort.uniq
+
+  if array.length > 2
+    mid_point = array.length / 2
+    root = TreeNode.new(array[mid_point])
+    root.left = build_tree(array[0...mid_point])
+    root.right = build_tree(array[mid_point + 1..- 1])
+  elsif array.length == 2
+    root = TreeNode.new(array[0])
+    root.left = nil
+    root.right = TreeNode.new(array[1])
+  elsif array.length == 1
+    root = TreeNode.new(array[0])
+  else
+    return nil
+  end
+
+  root
 end
 
-p build_tree(arr)
+p build_tree([1,2,3,4,5,6,7,8,9])
